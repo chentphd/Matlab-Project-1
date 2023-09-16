afc96ff2a2fa2cfc0633fd61964e704e09a2b82d
Chen <tonychen@umich.edu>
2023-09-16 00:07:11 -0400

    First commit GaussElimFile

---
+++ GaussElimFile.m
@@ -0,0 +1,63 @@
+% A = [2 1 -1; -3 -1 2; -2 1 2];
+% b = [8 ;-11 ;-3];
+% x = GaussElim(A, b);
+% disp('Solution x:');
+% disp(x);
+
+
+% Get matrix A from the user
+A = input('Enter the matrix A: ');
+% Get vector b from the user
+b = input('Enter the vector b: ');
+
+
+x = GaussElim(A, b);
+disp('Solution x:');
+disp(x);
+
+function x = GaussElim(A, b)
+%GAUSSELIM Takes as inputs a matrix A in R^nxn and a vector b in R^n. Using
+%Gaussian Elimination, returns the vector x in R^n such that Ax=b.
+%INPUTS: n x n Matrix A, n  x 1 vector b
+%OUTPUTS: n x 1 soltion vector x 
+
+    % Check if object A is matrix
+    if ~ismatrix(A)
+        error('Object A must be a matrix')
+    end
+
+    % Check if the matrix A is square
+    [m, n] = size(A);
+    if m ~= n
+        error('Matrix A must be square.');
+    end
+
+    % Check if the dimensions of A and b are compatible
+    if size(b,1) ~= n
+        error('Dimensions of A and b are not compatible. Run the program again');
+    end
+
+    % Augmented matrix [A | b]
+    Ab = [A, b];
+
+    % Perform forward elimination
+    % Divide by pivot, multiply negative factor and add to its col
+    % From AMS 510 
+    for i = 1:n
+        pivot = Ab(i,i);
+        Ab(i,:) = Ab(i,:) / pivot;
+        for j = i+1:n
+            factor = Ab(j,i);
+            Ab(j,:) = Ab(j,:) - factor * Ab(i,:);
+        end
+    end
+
+    % Perform back substitution
+    x = zeros(n, 1);
+    for i = n:-1:1
+        x(i) = Ab(i,n+1);
+        for j = i+1:n
+            x(i) = x(i) - Ab(i,j) * x(j);
+        end
+    end
+end
