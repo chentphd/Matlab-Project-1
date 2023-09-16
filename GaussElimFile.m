b40050f066452026aa78bb6b6459be44ed832a88
Chen <tonychen@umich.edu>
2023-09-16 12:30:51 -0400

    GaussElimFile bug fixes

--- GaussElimFile.m
+++ GaussElimFile.m
@@ -1,10 +1,10 @@
-% A = [2 1 -1; -3 -1 2; -2 1 2];
-% b = [8 ;-11 ;-3];
+% A = [1 -2 3; 3 -5 7; 7 6 11];
+% b = [5 ; 2 ; 2];
 % x = GaussElim(A, b);
 % disp('Solution x:');
 % disp(x);
 
-
+%Example input above 
 % Get matrix A from the user
 A = input('Enter the matrix A: ');
 % Get vector b from the user
@@ -23,19 +23,26 @@ function x = GaussElim(A, b)
 
     % Check if object A is matrix
     if ~ismatrix(A)
-        error('Object A must be a matrix')
+        error('Object A must be a matrix. Retype another matrix.')
     end
 
     % Check if the matrix A is square
     [m, n] = size(A);
     if m ~= n
-        error('Matrix A must be square.');
+        error('Matrix A must be square. Retype another matrix.');
     end
 
     % Check if the dimensions of A and b are compatible
     if size(b,1) ~= n
-        error('Dimensions of A and b are not compatible. Run the program again');
+        error('Dimensions of A and b are not compatible. Run the program again with appropriate dimensions');
     end
+
+    %Check if matirx is really non-singular. 
+    % We assume all matrix A is non-singular in order to have solution 
+    if det(A) == 0
+        error('Matrix is singular because Det A = 0. Retype another matrix.')
+    end
+
 
     % Augmented matrix [A | b]
     Ab = [A, b];
@@ -49,6 +56,7 @@ function x = GaussElim(A, b)
         for j = i+1:n
             factor = Ab(j,i);
             Ab(j,:) = Ab(j,:) - factor * Ab(i,:);
+            %Three operations: Divide, Multiply, subtract per step
         end
     end
 
